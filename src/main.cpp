08403bf02e4217c2e046183e9bded64fe6220bdb
k-main <kennkmaina@mgail.com>
2024-11-15 18:13:50 -0800

    established wireless communication btwn car and controller using simplified protocol

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,45 @@
 #include <RF24.h>
 #include <nRF24L01.h>
 
-#define NUM_TASKS 1
+#define NUM_TASKS 2
 
 #define BTN_1 32
-#define LED_1 33
+#define BTN_2 33
+// #define LED_1 33
 #define CE    4
 #define CSN   5
 #define SCK   18
 #define MOSI  23
+#define MISO  19
 
 const unsigned int GCD_PERIOD = 50;
 const unsigned int BTN_PERIOD = 50; 
-unsigned long timer = 0;
+const unsigned int NRF_PERIOD = 100;
 
 task tasks[NUM_TASKS];
 
 typedef enum btn_state { press, wait };
+char fwd_flag = 0;
+char bwd_flag = 0;
+// const char* MSG_BUFFER = "Hello from ESP32.";
+char MSG_BUFFER[16];
+const byte  PIPE_ADDR[6] = "00001";
+RF24 radio(4, 5);
 
 void updateTasks(void);
 int btn_func(int state);
+int nrf_func(int state);
+
 
 void setup() {
-  
-  pinMode(LED_1, OUTPUT);
+  // pinMode(LED_1, OUTPUT);
   pinMode(BTN_1, INPUT);
+  pinMode(BTN_2, INPUT);
+
+  radio.begin();
+  radio.openWritingPipe(PIPE_ADDR);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 
   Serial.begin(115200);
   unsigned int i = 0;
@@ -36,6 +51,11 @@ void setup() {
   tasks[i].state = wait;
   tasks[i].taskFunc = &btn_func;
   i++;
+  tasks[i].elapsedTime = 0;
+  tasks[i].period = 50;
+  tasks[i].state = wait;
+  tasks[i].taskFunc = &nrf_func;
+  i++;
 }
 
 void loop() {
@@ -43,13 +63,25 @@ void loop() {
   updateTasks();
 }
 
+int nrf_func(int state){
+  if (fwd_flag) {
+    MSG_BUFFER[0] = 'F';
+  } else if (bwd_flag) {
+    MSG_BUFFER[0] = 'B';
+  } else {
+    MSG_BUFFER[0] = 'N';
+  }
+
+  radio.write(&MSG_BUFFER, sizeof(MSG_BUFFER));
+  return state;
+}
+
 int btn_func(int state){
   switch(state){
     case wait:
 
-      if (digitalRead(BTN_1) > 0) {
+      if (digitalRead(BTN_1) > 0 || digitalRead(BTN_2) > 0) {
         state = press;
-        digitalWrite(LED_1, HIGH);
       } else {
         state = wait;
       }
@@ -57,11 +89,21 @@ int btn_func(int state){
     break;
     case press:
 
-      if (digitalRead(BTN_1) < 1) {
+      if (digitalRead(BTN_1) < 1 && digitalRead(BTN_2) < 1) {
         state = wait;
-        digitalWrite(LED_1, LOW);
+        bwd_flag = fwd_flag = 0;
       } else {
         state = press;
+        if (digitalRead(BTN_1)) {
+          Serial.println("Right button.");
+          bwd_flag = 0;
+          fwd_flag = 1;
+        }
+        if (digitalRead(BTN_2)) {
+          Serial.println("Left button.");
+          fwd_flag = 0;
+          bwd_flag = 1;
+        }
       }
 
     break;
