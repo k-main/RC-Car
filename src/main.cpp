add5a249a4125b687582fe98cc827b6c965524b6
k-main <kennkmaina@mgail.com>
2024-12-05 16:54:52 -0800

    One gate voltage drives two mosfets now, meaning motor driver works with half as many GPIO pins

--- src/main.cpp
+++ src/main.cpp
@@ -19,20 +19,17 @@ const byte PIPE_ADDR[6] = "00001";
 char RECV_BUFFER[16] = "";
 
 /* Motor Signal Output Pins */
-#define PMOS_01 21
-#define NMOS_02 22
-#define PMOS_03 17
-#define NMOS_04 16
-
-#define TEST_BTN1 32
-#define TEST_BTN2 33
+#define HB0_B0 21
+#define HB0_B1 22
+#define HB1_B0 17
+#define HB1_B1 16
 
 //d21 d22 d17(tx2) d16(rx2)
 
-const unsigned int GCD_PERIOD = 50;
+const unsigned int GCD_PERIOD = 10;
 const unsigned int IN_PERIOD  = 50;
-const unsigned int M_PERIOD   = 50;
-const unsigned int NRF_PERIOD = 1000;
+const unsigned int M_PERIOD   = 10;
+const unsigned int NRF_PERIOD = 100;
 
 task tasks[NUM_TASKS];
 
@@ -40,6 +37,7 @@ typedef enum switch_state {high, low};
 typedef enum input_state {wait, read_btn};
 typedef enum m_state {left, right, off};
 m_state motor_state;
+m_state car_rot;
  
 void updateTasks(void);
 int tick_in(int state);
@@ -47,28 +45,29 @@ int tick_m(int state);
 int tick_nrf(int state);
 
 inline void m_off(void){
-  digitalWrite(PMOS_01, HIGH);
-  digitalWrite(PMOS_03, HIGH);
-  digitalWrite(NMOS_02, LOW);
-  digitalWrite(NMOS_04, LOW);
+  digitalWrite(HB1_B0, HIGH);
+  digitalWrite(HB1_B1, HIGH);
+  digitalWrite(HB0_B0, HIGH);
+  digitalWrite(HB0_B1, HIGH);
 }
 
-inline void m_right(void);
+inline void m_fwd(void);
+inline void m_bwd(void);
 inline void m_left(void);
+inline void m_right(void);
 
 void setup() {
     motor_state = off;
+    car_rot = off;
     unsigned char i = 0;
     Serial.begin(115200);
     // DDRC = 0b1111;
     // PORTC = 0b0101;
-    pinMode(PMOS_01, OUTPUT);
-    pinMode(NMOS_02, OUTPUT);
-    pinMode(PMOS_03, OUTPUT);
-    pinMode(NMOS_04, OUTPUT);
 
-    pinMode(TEST_BTN1, INPUT);
-    pinMode(TEST_BTN2, INPUT);
+    pinMode(HB0_B0, OUTPUT);
+    pinMode(HB0_B1, OUTPUT);
+    pinMode(HB1_B0, OUTPUT);
+    pinMode(HB1_B1, OUTPUT);
 
     m_off();
 
@@ -114,32 +113,80 @@ void updateTasks(void){
 }
 
 
-
-
 inline void m_right(void){
-  m_off();
-  digitalWrite(PMOS_01, LOW);
-  digitalWrite(NMOS_04, HIGH);
-  // PORTC = 0b0101;
-  // digitalWrite(PMOS_03, HIGH);
-  // digitalWrite(NMOS_02, LOW);
-  // PORTC = 0b1100;
+  digitalWrite(HB1_B0, LOW);
+  digitalWrite(HB1_B1, HIGH);
+
+  digitalWrite(HB0_B0, LOW);
+  digitalWrite(HB0_B1, HIGH);
 }
 
 inline void m_left(void){
-  m_off();
-  digitalWrite(NMOS_02, HIGH);
-  digitalWrite(PMOS_03, LOW);
-  // PORTC = 0b0101;
-  // PORTC = 0b0011;
-  // digitalWrite(PMOS_01, HIGH);
-  // digitalWrite(NMOS_04, LOW);
+  digitalWrite(HB1_B0, HIGH);
+  digitalWrite(HB1_B1, LOW);
+
+  digitalWrite(HB0_B0, HIGH);
+  digitalWrite(HB0_B1, LOW);
+  
+}
+
+inline void m_fwd(void){
+
+
+  digitalWrite(HB1_B0, LOW);
+  digitalWrite(HB1_B1, HIGH);
+
+  digitalWrite(HB0_B0, HIGH);
+  digitalWrite(HB0_B1, LOW);
+
+}
+
+inline void m_bwd(void){
+
+  digitalWrite(HB1_B0, HIGH);
+  digitalWrite(HB1_B1, LOW);
+
+  digitalWrite(HB0_B0, LOW);
+  digitalWrite(HB0_B1, HIGH);
 }
 
 int tick_nrf(int state) {
   if (radio.available()){
     radio.read(&RECV_BUFFER, sizeof(RECV_BUFFER));
-    Serial.println((const char*)RECV_BUFFER);
+    int motor_ctrl = (int)RECV_BUFFER[0];
+    int jst_x = (int)RECV_BUFFER[1];
+    int jst_y = (int)RECV_BUFFER[2];
+    switch(motor_ctrl){
+      case 0:
+        Serial.println("Motors off.");
+        motor_state = off;
+        break;
+      case 1:
+        Serial.println("Backwards.");
+        motor_state = left;
+        break;
+      case 2:
+        Serial.println("Forward.");
+        motor_state = right;
+        break;
+    }
+
+    if (jst_y > 20) {
+      car_rot = right;
+    } else if (jst_y < 10) {
+      car_rot = left;
+    } else {
+      car_rot = off;
+    }
+
+    Serial.println(" ");
+
+    Serial.println(motor_ctrl);
+    Serial.print("Joystick X: ");
+    Serial.println((int)RECV_BUFFER[1]);
+
+    Serial.print("Joystick Y: ");
+    Serial.println((int)RECV_BUFFER[2]);
     memset(&RECV_BUFFER, 0, sizeof(RECV_BUFFER));
   }
 
@@ -151,36 +198,36 @@ int tick_in(int state) {
   short pin32, pin33;
   switch(state){
     case wait:
-      if (digitalRead(TEST_BTN1) > 0 || digitalRead(TEST_BTN2) > 0) {
-        state = read_btn;
-        break;
-      } else {
-        state = wait;
-        motor_state = off;
-      }
+      // if (digitalRead(TEST_BTN1) > 0 || digitalRead(TEST_BTN2) > 0) {
+      //   state = read_btn;
+      //   break;
+      // } else {
+      //   state = wait;
+      //   motor_state = off;
+      // }
     break;
     case read_btn:
-      pin32 = digitalRead(TEST_BTN1);
-      pin33 = digitalRead(TEST_BTN2);
+      // pin32 = digitalRead(TEST_BTN1);
+      // pin33 = digitalRead(TEST_BTN2);
 
-      if (pin32 == 1) {
-        if (pin33 == 1){
-          motor_state = off;
-          state = wait;
-          break;
-        } else {
-          motor_state = right;
-          state = read_btn;
-          break;
-        }
-      } else if (pin33 == 1) {
-        motor_state = left;
-        state = read_btn;
-        break;
-      } else {
-        state = wait;
-        motor_state = off;
-      }
+      // if (pin32 == 1) {
+      //   if (pin33 == 1){
+      //     motor_state = off;
+      //     state = wait;
+      //     break;
+      //   } else {
+      //     motor_state = right;
+      //     state = read_btn;
+      //     break;
+      //   }
+      // } else if (pin33 == 1) {
+      //   motor_state = left;
+      //   state = read_btn;
+      //   break;
+      // } else {
+      //   state = wait;
+      //   motor_state = off;
+      // }
     break;
   }
 
@@ -190,14 +237,24 @@ int tick_in(int state) {
 int tick_m(int state) {
   switch(motor_state){
     case left:
-      m_left();
+      m_bwd();
     break;
     case right:
-      m_right();
+      m_fwd();
     break;
     case off:
       m_off();
     break;
+  }
+  
+  if (motor_state == off) {
+    if (car_rot == left) {
+      m_left();
+    } else if (car_rot == right) {
+      m_right();
+    } else {
+      m_off();
+    }
   }
 
   return state;
