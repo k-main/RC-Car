6f380d9ed8f1737176de05701c52a72268870a1b
k-main <kennkmaina@mgail.com>
2024-12-16 00:32:51 -0800

    final motor driver commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include "SPI.h"
 #include "nRF24L01.h"
 #include "RF24.h"
+#include "Wire.h"
+#include "MPU6050.h"
 
 
-#define NUM_TASKS 3
+#define NUM_TASKS 2
 
 /* NRF24L01 Module SPI Pins & Setup */
 #define CE    4
@@ -19,17 +21,20 @@ const byte PIPE_ADDR[6] = "00001";
 char RECV_BUFFER[16] = "";
 
 /* Motor Signal Output Pins */
-#define HB0_B0 21
-#define HB0_B1 22
+#define HB0_B0 33
+#define HB0_B1 32
 #define HB1_B0 17
 #define HB1_B1 16
 
 //d21 d22 d17(tx2) d16(rx2)
 
+#define MPU_SDA 21
+#define MPU_SCL 22
+
 const unsigned int GCD_PERIOD = 1;
-const unsigned int IN_PERIOD  = 50;
+const unsigned int ACCEL_PERIOD = 200;
 const unsigned int M_PERIOD   = 1;
-const unsigned int NRF_PERIOD = 100;
+const unsigned int NRF_PERIOD = 20;
 
 task tasks[NUM_TASKS];
 
@@ -39,14 +44,22 @@ typedef enum m_state {left, right, off, forward, back};
 m_state car_dir;
 m_state rotation_dir;
 m_state motor_state;
- 
 double ON_X = 0, ON_Y = 0, PWM_PERIOD = 16;
 short PWM_X = 0, PWM_Y = 0;
+ 
+
+typedef enum sensor {read_val};
+short accel_read_cnt = 0, accel_read_max = 3;
+int32_t sumAx = 0, sumAy = 0, sumAz = 0;
+int32_t sumGx = 0, sumGy = 0, sumGz = 0;
+int16_t ax, ay, az, gx, gy, gz;
+MPU6050 mpu;
 
 void updateTasks(void);
-int tick_in(int state);
-int tick_m(int state);
-int tick_nrf(int state);
+int in_func(int state);
+int motor_func(int state);
+int nrf_func(int state);
+int accel_func(int state);
 
 inline void m_off(void){
   if (motor_state == off) return;
@@ -71,6 +84,21 @@ void setup() {
     unsigned char i = 0;
     Serial.begin(115200);
 
+    Wire.begin(MPU_SDA, MPU_SCL);
+    mpu.initialize();
+    if (mpu.testConnection()) {
+      Serial.println("MPU6050 connection successful");
+
+      // automatic calibration (leave the unit standing still until 5 seconds)
+      Serial.println("Calibrating gyroscope...");
+      mpu.CalibrateGyro(6);
+      Serial.println("Calibration complete.");
+      mpu.PrintActiveOffsets();
+    } else {
+      Serial.println("MPU6050 initialization failed.");
+      // while(1);
+    }
+
     radio.begin();
     radio.openReadingPipe(0,PIPE_ADDR);
     radio.setPALevel(RF24_PA_MIN);
@@ -89,18 +117,18 @@ void setup() {
     tasks[i].period = M_PERIOD;
     tasks[i].state = low;
     tasks[i].elapsedTime = tasks[i].period;
-    tasks[i].taskFunc = &tick_m;
-    i++;
-    tasks[i].period = IN_PERIOD;
-    tasks[i].state = wait;
-    tasks[i].elapsedTime = tasks[i].period;
-    tasks[i].taskFunc = &tick_in;
+    tasks[i].taskFunc = &motor_func;
     i++;
     tasks[i].period = NRF_PERIOD;
     tasks[i].state = read_btn;
     tasks[i].elapsedTime = tasks[i].period;
-    tasks[i].taskFunc = &tick_nrf;
+    tasks[i].taskFunc = &nrf_func;
     i++;
+    // tasks[i].period = ACCEL_PERIOD;
+    // tasks[i].state = read_val;
+    // tasks[i].elapsedTime = tasks[i].period;
+    // tasks[i].taskFunc = &accel_func;
+    // i++;
 }
 
 
@@ -159,41 +187,49 @@ inline void m_bwd(void){
   motor_state = back;
 }
 
-int tick_nrf(int state) {
+int accel_func(int state) {
+  for (char i = 0; i < accel_read_max; i++) {
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    sumAx += ax / accel_read_max;
+    sumAy += ay / accel_read_max;
+    sumAz += az / accel_read_max;
+    sumGx += gx / accel_read_max;
+    sumGy += gy / accel_read_max;
+    sumGz += gy / accel_read_max;
+  }
+  
+  // Serial.print("MPU6050: ");
+  // Serial.print(" Acc[X]="); Serial.print(sumAx);
+  // Serial.print(" Acc[Y]="); Serial.print(sumAy);
+  // Serial.print(" Acc[Z]="); Serial.print(sumAz);
+  // Serial.print(" Gyro[X]="); Serial.print(sumGx);
+  // Serial.print(" Gyro[Y]="); Serial.print(sumGy);
+  // Serial.print(" Gyro[Z]="); Serial.println(sumGz);
+
+  return state;
+}
+
+int nrf_func(int state) {
   if (radio.available()){
     radio.read(&RECV_BUFFER, sizeof(RECV_BUFFER));
     int motor_ctrl = (int)RECV_BUFFER[0];
     int jst_x = (int)RECV_BUFFER[1];
     int jst_y = (int)RECV_BUFFER[2];
-    // switch(motor_ctrl){
-    //   case 0:
-    //     Serial.print("Motors off.");
-    //     car_dir = off;
-    //     break;
-    //   case 1:
-    //     Serial.print("Backwards.");
-    //     car_dir = left;
-    //     break;
-    //   case 2:
-    //     Serial.print("Forward.");
-    //     car_dir = right;
-    //     break;
-    // }
 
-    if (jst_x > 16) {
+    if (jst_x > 17) {
       rotation_dir = right;
       ON_X = jst_x;
-    } else if (jst_x < 14) {
+    } else if (jst_x < 13) {
       rotation_dir = left;
       ON_X = 31 - jst_x;
     } else {
       rotation_dir = off;
     }
 
-    if (jst_y > 16) {
+    if (jst_y > 17) {
       car_dir = right;
       ON_Y = jst_y;
-    } else if (jst_y < 12) {
+    } else if (jst_y < 11) {
       car_dir = left;
       ON_Y = 31 - jst_y;
     } else {
@@ -211,8 +247,8 @@ int tick_nrf(int state) {
     Serial.print("Joystick Y: ");
     Serial.println((int)RECV_BUFFER[2]);
 
-    Serial.print("ON Y: ");
-    Serial.println(ON_Y);
+    // Serial.print("ON Y: ");
+    // Serial.println(ON_Y);
 
     memset(&RECV_BUFFER, 0, sizeof(RECV_BUFFER));
   }
@@ -221,47 +257,7 @@ int tick_nrf(int state) {
   return state;
 }
 
-int tick_in(int state) {
-  short pin32, pin33;
-  switch(state){
-    case wait:
-      // if (digitalRead(TEST_BTN1) > 0 || digitalRead(TEST_BTN2) > 0) {
-      //   state = read_btn;
-      //   break;
-      // } else {
-      //   state = wait;
-      //   car_dir = off;
-      // }
-    break;
-    case read_btn:
-      // pin32 = digitalRead(TEST_BTN1);
-      // pin33 = digitalRead(TEST_BTN2);
-
-      // if (pin32 == 1) {
-      //   if (pin33 == 1){
-      //     car_dir = off;
-      //     state = wait;
-      //     break;
-      //   } else {
-      //     car_dir = right;
-      //     state = read_btn;
-      //     break;
-      //   }
-      // } else if (pin33 == 1) {
-      //   car_dir = left;
-      //   state = read_btn;
-      //   break;
-      // } else {
-      //   state = wait;
-      //   car_dir = off;
-      // }
-    break;
-  }
-
-  return state;
-}
-
-int tick_m(int state) {
+int motor_func(int state) {
   switch(car_dir){
     case left:
       PWM_Y = (PWM_Y > 31) ? 0 : PWM_Y + 1;
@@ -288,6 +284,7 @@ int tick_m(int state) {
   if (car_dir == off) {
     switch(rotation_dir){
       case left:
+      PWM_X = (PWM_X > 31) ? 0 : PWM_X + 1;
       if (PWM_X <= ON_X) {
         m_left();
       } else {
@@ -295,14 +292,17 @@ int tick_m(int state) {
       }
       break; 
       case right:
-      PWM_X = (PWM_X > 31) ? 0 : PWM_X + 1;;
+      PWM_X = (PWM_X > 31) ? 0 : PWM_X + 1;
       if (PWM_X <= ON_X) {
         m_right();
       } else {
         m_off();
       }
       break;
+      case off:
+      PWM_X = 0;
       m_off();
+      break;
     }
 
   }
